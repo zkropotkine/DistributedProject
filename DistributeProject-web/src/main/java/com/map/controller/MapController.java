0eae443fef7e1d335710f9e5efa60028e04578da
citlalig <citlalig@Citlalis-MacBook-Air.local>
2013-10-30 22:57:59 -0600

    add new markers

--- DistributeProject-web/src/main/java/com/map/controller/MapController.java
+++ DistributeProject-web/src/main/java/com/map/controller/MapController.java
@@ -8,10 +8,14 @@ import java.io.Serializable;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import org.primefaces.event.map.PointSelectEvent;
 import org.primefaces.event.map.StateChangeEvent;
+import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.LatLngBounds;
+import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Marker;
 
 /**
  *
@@ -19,6 +23,14 @@ import org.primefaces.model.map.LatLngBounds;
  */
 @ManagedBean
 public class MapController implements Serializable  {
+    private MapModel markerModel; 
+    private double lat;  
+    private double lng;  
+    private String title;
+    
+    public MapController() {  
+        markerModel = new DefaultMapModel();  
+    }  
       
     public void onStateChange(StateChangeEvent event) {  
         LatLngBounds bounds = event.getBounds();  
@@ -35,8 +47,71 @@ public class MapController implements Serializable  {
           
         addMessage(new FacesMessage(FacesMessage.SEVERITY_INFO, "Point Selected", "Lat:" + latlng.getLat() + ", Lng:" + latlng.getLng()));  
     }  
+    
+    public void addMarker(ActionEvent actionEvent) {  
+        Marker marker = new Marker(new LatLng(lat, lng), title);  
+        markerModel.addOverlay(marker);  
+          
+        addMessage(new FacesMessage(FacesMessage.SEVERITY_INFO, "Marker Added", "Lat:" + lat + ", Lng:" + lng));  
+    }  
       
     public void addMessage(FacesMessage message) {  
         FacesContext.getCurrentInstance().addMessage(null, message);  
     }  
+
+    /**
+     * @return the emptyModel
+     */
+    public MapModel getMarkerModel() {
+        return markerModel;
+    }
+
+    /**
+     * @param emptyModel the emptyModel to set
+     */
+    public void setMarkerModel(MapModel markerModel) {
+        this.markerModel = markerModel;
+    }
+
+    /**
+     * @return the lat
+     */
+    public double getLat() {
+        return lat;
+    }
+
+    /**
+     * @param lat the lat to set
+     */
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    /**
+     * @return the lng
+     */
+    public double getLng() {
+        return lng;
+    }
+
+    /**
+     * @param lng the lng to set
+     */
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
